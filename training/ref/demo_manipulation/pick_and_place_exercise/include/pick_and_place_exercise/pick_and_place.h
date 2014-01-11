d29070dd85db8688e28526c6adfe30319d85553b
ROS-Industrial <ros-industrial@ROS-1.(none)>
2014-01-10 18:02:57 -0600

    added new function definitions for pickup_box and place_box

--- training/ref/demo_manipulation/pick_and_place_exercise/include/pick_and_place_exercise/pick_and_place.h
+++ training/ref/demo_manipulation/pick_and_place_exercise/include/pick_and_place_exercise/pick_and_place.h
@@ -24,11 +24,32 @@ extern pick_and_place_config cfg;
 
 // =============================== Task Functions ===============================
 void move_to_wait_position(move_group_interface::MoveGroup& move_group);
+
 void set_gripper(GraspActionClient& grasp_action_client, bool do_grasp);
+
 geometry_msgs::Pose detect_box_pick(tf::TransformListener &tf_listener);
-std::vector<geometry_msgs::Pose> create_pick_moves(tf::TransformListener &tf_listener, geometry_msgs::Pose &box_pose);
-void move_through_pick_poses(move_group_interface::MoveGroup& move_group, GraspActionClient& grasp_action_client,std::vector<geometry_msgs::Pose>& pick_poses);
+
+std::vector<geometry_msgs::Pose> create_pick_moves(tf::TransformListener &tf_listener,
+		geometry_msgs::Pose &box_pose);
+
+void move_through_pick_poses(move_group_interface::MoveGroup& move_group,
+		GraspActionClient& grasp_action_client,
+		std::vector<geometry_msgs::Pose>& pick_poses);
+
 std::vector<geometry_msgs::Pose> create_place_moves(tf::TransformListener& tf_listener);
-void move_through_place_poses(move_group_interface::MoveGroup& move_group, GraspActionClient& grasp_action_client,std::vector<geometry_msgs::Pose>& place_poses);
+
+void move_through_place_poses(move_group_interface::MoveGroup& move_group,
+		GraspActionClient& grasp_action_client,
+		std::vector<geometry_msgs::Pose>& place_poses);
+
+void pickup_box(move_group_interface::MoveGroup& move_group,
+		ros::Publisher& attached_object_publisher,
+		GraspActionClient& grasp_action_client,
+		std::vector<geometry_msgs::Pose>& pick_poses);
+
+void place_box(move_group_interface::MoveGroup& move_group,
+		ros::Publisher& attached_object_publisher,
+		GraspActionClient& grasp_action_client,
+		std::vector<geometry_msgs::Pose>& place_poses);
 
 #endif /* PICK_AND_PLACE_H_ */
