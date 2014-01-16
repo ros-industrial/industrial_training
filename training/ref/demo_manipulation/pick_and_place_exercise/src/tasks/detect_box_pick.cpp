9db5681adf73a62c18d9950709b7008379601041
ROS-Industrial <ros-industrial@ROS-1.(none)>
2014-01-15 18:10:22 -0600

    uses sensor data to detect height

--- training/ref/demo_manipulation/pick_and_place_exercise/src/tasks/detect_box_pick.cpp
+++ training/ref/demo_manipulation/pick_and_place_exercise/src/tasks/detect_box_pick.cpp
@@ -6,6 +6,14 @@
  */
 
 #include <pick_and_place_exercise/pick_and_place.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <pcl/ros/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/common/centroid.h>
+#include <pcl_ros/transforms.h>
 
 /* DETECTING BOX PICK POSE
   Goal:
@@ -32,6 +40,56 @@ geometry_msgs::Pose detect_box_pick(tf::TransformListener &tf_listener)
   // save pose in 'box_pose'
   /* Fill Code: [ use the 'tf::poseTFToMsg' to convert a TF transform into a pose message] */
   tf::poseTFToMsg(world_to_box_pick_tf,box_pose);
+
+  // detecting height
+  pcl::PointCloud<pcl::PointXYZ>::Ptr raw_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  sensor_msgs::PointCloud2ConstPtr sensor_cloud =
+		  ros::topic::waitForMessage<sensor_msgs::PointCloud2>(cfg.POINT_CLOUD_TOPIC,ros::Duration(2.0f));
+
+  if(sensor_cloud == sensor_msgs::PointCloud2ConstPtr())
+  {
+	  // no point cloud message received, defaulting to box height
+	  box_pose.position.z = cfg.BOX_SIZE.getZ();
+	  ROS_INFO_STREAM("No point cloud message received, using box height "<<cfg.BOX_SIZE.getZ());
+  }
+  else
+  {
+	  ROS_INFO_STREAM("point cloud message received, estimating surface");
+
+	  // converting message to point cloud type
+	  pcl::fromROSMsg<pcl::PointXYZ>(*sensor_cloud,*raw_cloud);
+
+	  // applying transform
+	  pcl_ros::transformPointCloud<pcl::PointXYZ>(cfg.WORLD_FRAME_ID,
+			  *raw_cloud,*cloud,tf_listener);
+
+	  // applying filter in x axis
+	  float min = box_pose.position.x - 0.2*cfg.BOX_SIZE.x();
+	  float max = box_pose.position.x + 0.2*cfg.BOX_SIZE.x();
+	  pcl::PassThrough<pcl::PointXYZ> filter;
+	  filter.setInputCloud(cloud);
+	  filter.setFilterFieldName("x");
+	  filter.setFilterLimits(min,max);
+	  filter.filter(*cloud);
+
+	  // applying filter in y axis
+	  min = box_pose.position.y - 0.2*cfg.BOX_SIZE.y();
+	  max = box_pose.position.y + 0.2*cfg.BOX_SIZE.y();
+	  filter.setInputCloud(cloud);
+	  filter.setFilterFieldName("y");
+	  filter.setFilterLimits(min,max);
+	  filter.filter(*cloud);
+
+	  // computing centroid
+	  Eigen::Vector4f centroid;
+	  pcl::compute3DCentroid(*cloud,centroid);
+
+	  // setting z value
+	  box_pose.position.z = centroid[2];
+  }
+
+  set_object_in_world(true,box_pose);
 
   return box_pose;
 }
