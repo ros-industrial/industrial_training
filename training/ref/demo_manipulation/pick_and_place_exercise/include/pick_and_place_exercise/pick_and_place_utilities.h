59166f1d00bcdcea93937b75d8ad3d578f22aa89
ROS-Industrial <ros-industrial@ROS-1.(none)>
2014-01-16 18:02:51 -0600

    replaced tag frame for box pick configuration variable

--- training/ref/demo_manipulation/pick_and_place_exercise/include/pick_and_place_exercise/pick_and_place_utilities.h
+++ training/ref/demo_manipulation/pick_and_place_exercise/include/pick_and_place_exercise/pick_and_place_utilities.h
@@ -40,7 +40,7 @@ public:
   std::string TCP_LINK_NAME;   // Link / frame name for the suction gripper tool-tip
   std::string WRIST_LINK_NAME; // Link / frame name for the robot wrist tool-flange
   std::string WORLD_FRAME_ID;  // Frame name for the fixed world reference frame
-  std::string TAG_FRAME_ID;    // Frame name for the "AR Tag" mounted to the target box
+  std::string BOX_PICK_FRAME_ID;    // Frame name for the "AR Tag" mounted to the target box
   std::string HOME_POSE_NAME;  // Named pose for robot Home position (set in SRDF)
   std::string WAIT_POSE_NAME;  // Named pose for robot WAIT position (set in SRDF)
   tf::Vector3 BOX_SIZE;        // Size of the target box
@@ -56,6 +56,7 @@ public:
     ARM_GROUP_NAME  = "manipulator";
     TCP_LINK_NAME   = "tcp_frame";
     ATTACHED_OBJECT_TOPIC = "attached_collision_object";
+    COLLISION_OBJECT_TOPIC = "collision_object";
     POINT_CLOUD_TOPIC = "/kinect/depth_registered/points";
     MARKER_TOPIC = "pick_and_place_marker";
     ATTACHED_OBJECT_ID = "attached_object";
@@ -63,7 +64,7 @@ public:
     WORLD_FRAME_ID  = "world_frame";
     HOME_POSE_NAME  = "home";
     WAIT_POSE_NAME  = "wait";
-    TAG_FRAME_ID    = "ar_tag";
+    BOX_PICK_FRAME_ID    = "box_pick_frame";
     GRASP_ACTION_NAME = "grasp_execution_action";
     BOX_SIZE        = tf::Vector3(0.1f, 0.1f, 0.1f);
     BOX_PICK_TF     = tf::Transform(tf::Quaternion::getIdentity(), tf::Vector3(-0.8f,0.2f,BOX_SIZE.getZ()));
