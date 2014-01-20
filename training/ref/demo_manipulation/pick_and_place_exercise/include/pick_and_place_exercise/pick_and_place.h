2c247e621314ff1b5651facf9d7ccfe3d7646a60
ROS-Industrial <ros-industrial@ROS-1.(none)>
2014-01-20 13:33:49 -0600

    removed the attached_object_publisher input argument from the th pick and place box functions

--- training/ref/demo_manipulation/pick_and_place_exercise/include/pick_and_place_exercise/pick_and_place.h
+++ training/ref/demo_manipulation/pick_and_place_exercise/include/pick_and_place_exercise/pick_and_place.h
@@ -11,6 +11,7 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <moveit/move_group_interface/move_group.h>
+#include <moveit_msgs/PlanningScene.h>
 #include <object_manipulation_msgs/GraspHandPostureExecutionAction.h>
 #include <tf/transform_listener.h>
 
@@ -24,6 +25,7 @@ extern pick_and_place_config cfg;
 extern ros::Publisher marker_publisher;
 extern ros::Publisher collision_object_publisher;
 extern ros::Publisher attach_object_publisher;
+extern ros::Publisher planning_scene_publisher;
 
 // =============================== Task Functions ===============================
 void move_to_wait_position(move_group_interface::MoveGroup& move_group);
@@ -32,7 +34,7 @@ void set_gripper(GraspActionClient& grasp_action_client, bool do_grasp);
 
 void set_attached_object(bool attach,const geometry_msgs::Pose &pose = geometry_msgs::Pose());
 
-void set_object_in_world(bool add,const geometry_msgs::Pose &pose= geometry_msgs::Pose());
+void reset_world();
 
 geometry_msgs::Pose detect_box_pick();
 
@@ -50,12 +52,10 @@ void move_through_place_poses(move_group_interface::MoveGroup& move_group,
 		std::vector<geometry_msgs::Pose>& place_poses);
 
 void pickup_box(move_group_interface::MoveGroup& move_group,
-		ros::Publisher& attached_object_publisher,
 		GraspActionClient& grasp_action_client,
 		std::vector<geometry_msgs::Pose>& pick_poses,const geometry_msgs::Pose& box_pose);
 
 void place_box(move_group_interface::MoveGroup& move_group,
-		ros::Publisher& attached_object_publisher,
 		GraspActionClient& grasp_action_client,
 		std::vector<geometry_msgs::Pose>& place_poses);
 
