f77f54bc9a2edccb36b405e2ab96575e212cbd66
ROS-Industrial <ros-industrial@ROS-1.(none)>
2014-02-21 15:39:58 -0600

    reverted back to modifying allowed collision matrix

--- training/ref/demo_manipulation/collision_avoidance_pick_and_place/src/tasks/place_box.cpp
+++ training/ref/demo_manipulation/collision_avoidance_pick_and_place/src/tasks/place_box.cpp
@@ -23,36 +23,17 @@ void collision_avoidance_pick_and_place::PickAndPlace::place_box(std::vector<geo
   // task variables
   bool success;
 
-
-
   // set the referenceFrame and EndEffectorLink
   /* Fill Code: [ use the 'setEndEffectorLink' and 'setPoseReferenceFrame' methods of 'move_group'] */
   move_group_ptr->setEndEffectorLink(cfg.WRIST_LINK_NAME);
   move_group_ptr->setPoseReferenceFrame(cfg.WORLD_FRAME_ID);
 
-  // set allowed planning time
-  move_group_ptr->setPlanningTime(60.0f);
-
   // move the robot to each wrist place pose
   for(unsigned int i = 0; i < place_poses.size(); i++)
   {
-  	moveit_msgs::RobotState robot_state;
-  	if(i==0 || i == 1)
-  	{
-      // attaching box
-      set_attached_object(true,box_pose,robot_state);
-
-    	ROS_INFO_STREAM("Robot State with attached payload at place move ["<<i<<"] :\n"<<robot_state);
-  	}
-  	else
-  	{
-      // detaching box
-      set_attached_object(false,geometry_msgs::Pose(),robot_state);
-  	}
-
   	// create motion plan
     move_group_interface::MoveGroup::Plan plan;
-    success = create_motion_plan(place_poses[i],robot_state,plan) && move_group_ptr->execute(plan);
+    success = create_motion_plan(place_poses[i],plan) && move_group_ptr->execute(plan);
 
     if(success)
     {
@@ -72,8 +53,13 @@ void collision_avoidance_pick_and_place::PickAndPlace::place_box(std::vector<geo
 	/*   - only call this once, after the target position has been reached */
 	/*   - HINT: this should be the second pose in the sequence of place_poses */
       set_gripper(false);
-
     }
+
+  	if(i==0 )
+  	{
+      // attaching box
+      set_attached_object(false);
+  	}
 
   }
 }
