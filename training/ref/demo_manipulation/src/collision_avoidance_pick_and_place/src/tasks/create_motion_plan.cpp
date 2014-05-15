aed3a9d2d4d6f0078ee6c021d85552a62e2aa3c4
ros-industrial <ros-industrial@rosindustrial-HP-Z400-Workstation.(none)>
2014-05-15 11:05:58 -0500

    sets is_diff to true in motion plan request

--- training/ref/demo_manipulation/src/collision_avoidance_pick_and_place/src/tasks/create_motion_plan.cpp
+++ training/ref/demo_manipulation/src/collision_avoidance_pick_and_place/src/tasks/create_motion_plan.cpp
@@ -39,11 +39,11 @@ bool PickAndPlace::create_motion_plan(const geometry_msgs::Pose &pose_target,
 			orientation_tolerances);
 
 	req.start_state = start_robot_state;
-	req.start_state.is_diff = false;
+	req.start_state.is_diff = true;
 	req.group_name = cfg.ARM_GROUP_NAME;
 	req.goal_constraints.push_back(pose_goal);
 	req.allowed_planning_time = 60.0f;
-	req.num_planning_attempts = 20;
+	req.num_planning_attempts = 10;
 
 	// request motion plan
 	bool success = false;
