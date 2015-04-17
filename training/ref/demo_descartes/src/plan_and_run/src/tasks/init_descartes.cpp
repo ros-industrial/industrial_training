bfa7c45af1056a97ee02d3dbdcad852585a5d2d0
jrgnicho <ros-industrial@domain.org>
2015-04-17 18:21:50 -0500

    started documenting demo_descartes lab exercises

--- training/ref/demo_descartes/src/plan_and_run/src/tasks/init_descartes.cpp
+++ training/ref/demo_descartes/src/plan_and_run/src/tasks/init_descartes.cpp
@@ -1,12 +1,42 @@
 #include <plan_and_run/demo_application.h>
 
+/* INIT DESCARTES
+  Goal:
+    - Initialize a Descartes RobotModel object for carrying out various robot related tasks.
+    - Initialize a Descartes Path Planner for planning a robot path from a trajectory.
+
+  Hints:
+    - Use the "config_" structure in order to access the application parameters
+      needed to initialize the Descartes RobotModel object
+*/
+
 namespace plan_and_run
 {
 
 void DemoApplication::initDescartes()
 {
-  // Robot Model initialization
+  //ROS_ERROR_STREAM("Task '"<<__FUNCTION__ <<"' is incomplete. Exiting"); exit(-1);
+
+  // Instantiating a robot model
   robot_model_ptr_.reset(new ur5_demo_descartes::UR5RobotModel());
+
+  /*  Fill Code:
+   * Goal:
+   * - Initialize the "robot_model_ptr" variable by passing the required application parameters
+   *    into its "initialize" method.
+   * Hint:
+   * - The config_ structure contains the variables needed by the robot model
+   * - The "initialize" method takes the following arguments in this order
+   *    a - robot description string
+   *    b - group_name string.
+   *    c - world_frame string
+   *    d - tip_link string.
+   */
+/*  if(robot_model_ptr_->initialize(ROBOT_DESCRIPTION_PARAM,
+                                  "[ COMPLETE HERE ]",
+                                  "[ COMPLETE HERE ]",
+                                  "[ COMPLETE HERE ]"))*/
+
   if(robot_model_ptr_->initialize(ROBOT_DESCRIPTION_PARAM,
                                   config_.group_name,
                                   config_.world_frame,
@@ -20,8 +50,15 @@ void DemoApplication::initDescartes()
     exit(-1);
   }
 
-  // Planner initialization
-  if(planner_.initialize(robot_model_ptr_))
+  /*  Fill Code:
+   * Goal:
+   * - Initialize the Descartes path planner by calling "planner_.initialize(...)".
+   * - Pass the robot_model_ptr_ created earlier into the initialize method and save the result
+   *    into the "succeeded" variable.
+   */
+  //bool succeeded = false  /* [ COMPLETE HERE ]*/;
+  bool succeeded = planner_.initialize(robot_model_ptr_);
+  if(succeeded)
   {
     ROS_INFO_STREAM("Descartes Dense Planner initialized");
   }
@@ -30,6 +67,8 @@ void DemoApplication::initDescartes()
     ROS_ERROR_STREAM("Failed to initialize Dense Planner");
     exit(-1);
   }
+
+  ROS_INFO_STREAM("Task '"<<__FUNCTION__<<"' completed");
 
 }
 
