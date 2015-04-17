62a9682573e69af4ab58c14f826b94f0e0061d36
jrgnicho <ros-industrial@domain.org>
2015-04-17 18:21:50 -0500

    started documenting demo_descartes lab exercises

--- training/ref/demo_descartes/src/plan_and_run/src/tasks/generate_trajectory.cpp
+++ training/ref/demo_descartes/src/plan_and_run/src/tasks/generate_trajectory.cpp
@@ -1,18 +1,25 @@
 #include <plan_and_run/demo_application.h>
 
+/* GENERATE TRAJECTORY
+  Goal:
+    - Create a Descartes Trajectory from an array of poses.
+    - Create trajectory points that are free to rotate about the tool's z axis
+*/
+
 namespace plan_and_run
 {
 
 void DemoApplication::generateTrajectory(DescartesTrajectory& traj)
 {
+  //ROS_ERROR_STREAM("Task '"<<__FUNCTION__ <<"' is incomplete. Exiting"); exit(-1);
 
   using namespace descartes_core;
   using namespace descartes_trajectory;
 
-  // generating lemniscate curve
+
+  // generating trajectory using a lemniscate curve function.
   EigenSTL::vector_Affine3d poses;
   Eigen::Vector3d center(config_.center[0],config_.center[1],config_.center[2]);
-
   if(createLemniscateCurve(config_.foci_distance,config_.radius,config_.num_points,
                         config_.num_lemniscates,center,poses))
   {
@@ -27,6 +34,7 @@ void DemoApplication::generateTrajectory(DescartesTrajectory& traj)
   // publishing trajectory poses for visualization
   publishPosesMarkers(poses);
 
+
   // creating descartes trajectory points
   traj.clear();
   traj.reserve(poses.size());
@@ -34,13 +42,32 @@ void DemoApplication::generateTrajectory(DescartesTrajectory& traj)
   {
     const Eigen::Affine3d& pose = poses[i];
 
-    // creating cartesian point with rotational freedom about the tool's approach axis
+    /*  Fill Code:
+     * Goal:
+     * - Create AxialSymetricPt objects in order to define a trajectory cartesian point with
+     *    rotational freedom about the tool's z axis.
+     *
+     * Hint:
+     * - The point can be constructed as follows:
+     *
+     *    new AxialSymmetricPt(Pose ,Increment, Free Axis)
+     *
+     * - The Pose can be found in the for loop's "pose" variable.
+     * - The Increment can be found in the "ORIENTATION_INCREMENT" global variable.
+     * - The Free Axis can be selected from the AxialSymmetricPt::FreeAxis::Z_AXIS enumeration constants.
+     *
+     */
+    //descartes_core::TrajectoryPtPtr pt = descartes_core::TrajectoryPtPtr(/*[ COMPLETE HERE*/);
+
     descartes_core::TrajectoryPtPtr pt = descartes_core::TrajectoryPtPtr(
         new descartes_trajectory::AxialSymmetricPt(pose,ORIENTATION_INCREMENT,
                                                    descartes_trajectory::AxialSymmetricPt::FreeAxis::Z_AXIS) );
 
+    // saving points into trajectory
     traj.push_back(pt);
   }
+
+  ROS_INFO_STREAM("Task '"<<__FUNCTION__<<"' completed");
 
 }
 
