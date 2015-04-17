b1dda522f5da737f55704733f9ec2f0f0ea91857
Jeremy Zoss <jzoss@swri.org>
2015-04-16 20:20:05 -0400

    update Exercise 2.3 (launch files)

--- training/work/2.3/src/lesson_launch/src/jointaction_client.cpp
+++ training/work/2.3/src/lesson_launch/src/jointaction_client.cpp
@@ -1,44 +1,44 @@
-#include <stdio.h>      /* printf, scanf, puts, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
-
 #include <ros/ros.h>
 #include <actionlib/client/action_client.h>
-
-#include <trajectory_msgs/JointTrajectory.h>
-#include <control_msgs/FollowJointTrajectoryAction.h>
 #include <industrial_utils/param_utils.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
 
-int main (int argc, char **argv)
+typedef actionlib::ActionClient<control_msgs::FollowJointTrajectoryAction> ActionClient;
+
+std::vector<std::string> getJointNames()
 {
-  ros::init(argc, argv, "jointaction_client");
-  ros::NodeHandle nh;
+  #define PARAM_NAME "controller_joint_names"
+  std::vector<std::string> joint_names;
 
   //get joint names and add to trajectory
+  if (!industrial_utils::param::getJointNames(PARAM_NAME, "", joint_names))
+    ROS_WARN("Unable to read %s param.  Using standard 6-DOF joint names.", PARAM_NAME);
+  else
+    ROS_INFO("Loaded %d joint names from %s parameter", joint_names.size(), PARAM_NAME);
+
+  return joint_names;
+}
+
+trajectory_msgs::JointTrajectory calcTrajectory()
+{
   trajectory_msgs::JointTrajectory trajectory;
-  if (!industrial_utils::param::getJointNames("controller_joint_names", "", trajectory.joint_names))
-  {
-    ROS_WARN("Unable to read 'controller_joint_names' param.  Using standard 6-DOF joint names.");
-  }
-  ROS_INFO_STREAM("Loaded " << trajectory.joint_names.size() << " joint names from 'controller_joint_names' parameter");
 
-
-  srand (time(NULL));
+  trajectory.joint_names = getJointNames();
+  int nJnts = trajectory.joint_names.size();
 
   //Complete joint trajectory by oscillating each joint
   double amplitude(1.0);	//max allowable 1/2 angle, in radians
   ros::Duration pt_time_interval(0.1);
-  ros::Duration trajectory_time(rand()%60 + 30);
-  ROS_INFO_STREAM("Creating trajectory that will last for " << trajectory_time.toSec() << " seconds, with an amplitude of +/- " << amplitude << " radians.");
+  ros::Duration trajectory_time(30);
 
   trajectory_msgs::JointTrajectoryPoint pt;
-  pt.positions = std::vector<double>(trajectory.joint_names.size(), 0.0);
-  pt.velocities = std::vector<double>(trajectory.joint_names.size(), 0.0);
+  pt.positions = std::vector<double>(nJnts, 0.0);
+  pt.velocities = std::vector<double>(nJnts, 0.0);
   pt.time_from_start = ros::Duration(0.);
 
   while (pt.time_from_start < trajectory_time) {
 	  pt.time_from_start += pt_time_interval;
-	  for (unsigned int ii=0; ii<trajectory.joint_names.size(); ii++) {
+	  for (unsigned int ii=0; ii<nJnts; ii++) {
 		  double last_pt = pt.positions[ii];
 		  pt.positions[ii] = amplitude * sin(pt.time_from_start.toSec());
 		  pt.velocities[ii] = std::fabs(pt.positions[ii] - last_pt) / pt_time_interval.toSec();
@@ -46,16 +46,24 @@ int main (int argc, char **argv)
 	  trajectory.points.push_back(pt);
   }
 
+  return trajectory;
+}
+
+int main (int argc, char **argv)
+{
+  ros::init(argc, argv, "jointaction_client");
+  ros::NodeHandle nh;
+
   // create the action client
-  actionlib::ActionClient<control_msgs::FollowJointTrajectoryAction> ac(nh, "joint_trajectory_action");
-  ros::Rate(1.).sleep();    //wait for 5 sec to connect, rather than spin a thread to check connection
+  ActionClient ac(nh, "joint_trajectory_action");
+  ros::Rate(1.).sleep();    //wait for 1 sec to connect
 
-  ROS_INFO("Sending goal to action server.");
   // send trajectory to the action server
-  control_msgs::FollowJointTrajectoryActionGoal goal;
-  goal.goal.trajectory = trajectory;
-  ac.sendGoal(goal.goal);
+  ROS_INFO("Sending goal to action server.");
+  control_msgs::FollowJointTrajectoryGoal goal;
+  goal.trajectory = calcTrajectory();
+  ac.sendGoal(goal);
 
-  //exit
+  //don't wait for action to complete
   return 0;
 }
