f7044e3f0a878b25bacfa68ac4f5a91a44051b5f
jrgnicho <jrgnichodevel@gmail.com>
2015-04-30 22:26:57 -0500

    uncommented lines that were left commented out

--- training/work/demo_descartes/src/plan_and_run/src/tasks/load_parameters.cpp
+++ training/work/demo_descartes/src/plan_and_run/src/tasks/load_parameters.cpp
@@ -39,11 +39,11 @@ void DemoApplication::loadParameters()
    */
   if(ph.getParam("group_name",config_.group_name) &&
 
-      //ph.getParam("[ COMPLETE HERE ]",config_.tip_link) &&
+      ph.getParam("[ COMPLETE HERE ]",config_.tip_link) &&
 
       ph.getParam("base_link",config_.base_link) &&
 
-      //ph.getParam("[ COMPLETE HERE ]",config_.world_frame) &&
+      ph.getParam("[ COMPLETE HERE ]",config_.world_frame) &&
 
       ph.getParam("trajectory/time_delay",config_.time_delay) &&
       ph.getParam("trajectory/foci_distance",config_.foci_distance) &&
