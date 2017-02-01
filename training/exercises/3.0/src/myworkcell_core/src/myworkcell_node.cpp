9408a45fcfe5b8dc32ba058e3c45b0bfcbda132d
Austin Deric <austin.deric@swri.org>
2017-02-01 09:58:12 -0600

    incomplete.  removed training.bash and updated exercise numbering.

--- training/exercises/3.0/src/myworkcell_core/src/myworkcell_node.cpp
+++ training/exercises/3.0/src/myworkcell_core/src/myworkcell_node.cpp
@@ -9,12 +9,11 @@ public:
     vision_client_ = nh.serviceClient<myworkcell_core::LocalizePart>("localize_part");
   }
 
-  void start(const std::string& base_frame)
+  void start()
   {
     ROS_INFO("Attempting to localize part");
     // Localize the part
     myworkcell_core::LocalizePart srv;
-    srv.base_frame = base_frame;
     if (!vision_client_.call(srv))
     {
       ROS_ERROR("Could not localize part");
@@ -32,22 +31,17 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "myworkcell_node");
   ros::NodeHandle nh;
-  ros::NodeHandle pnh ("~");
   ros::AsyncSpinner async_spinner (1);
 
   // Hello World
   ROS_INFO("Hello, World from a ROS Node");
-
-  // Load parameters
-  std::string base_frame; // for localizing the part
-  pnh.param<std::string>("base_frame", base_frame, "world");
 
   ScanNPlan app (nh);
 
   ros::Duration(.5).sleep();
 
   async_spinner.start();
-  app.start(base_frame);
+  app.start();
 
   ros::spin();
 }
