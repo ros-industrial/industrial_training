6ba45138b8701cba4f48f516231ab81953bd917d
Jeremy Zoss <jzoss@swri.org>
2017-02-10 20:17:45 -0600

    Update Exercise 1.4 to match wiki

--- exercises/1.4/src/myworkcell_core/src/vision_node.cpp
+++ exercises/1.4/src/myworkcell_core/src/vision_node.cpp
@@ -1,37 +1,41 @@
-// Covers Topics/Messages
-// TF publishing
-// Service Server
-
+/**
+**  Simple ROS Node
+**/
 #include <ros/ros.h>
-#include <std_msgs/String.h>
-#include <tf/transform_listener.h>
 #include <fake_ar_publisher/ARMarker.h>
 
 class Localizer
 {
 public:
-  Localizer(ros::NodeHandle& nh)
-  {
-    ar_sub_ = nh.subscribe<fake_ar_publisher::ARMarker>("ar_pose_marker", 1, 
-      &Localizer::visionCallback, this);
-  }
+    Localizer(ros::NodeHandle& nh)
+    {
+        ar_sub_ = nh.subscribe<fake_ar_publisher::ARMarker>("ar_pose_marker", 1,
+        &Localizer::visionCallback, this);
+    }
 
-  void visionCallback(const fake_ar_publisher::ARMarkerConstPtr& msg)
-  {
-    last_msg_ = msg;
-  }
-  
-  ros::Subscriber ar_sub_;
-  fake_ar_publisher::ARMarkerConstPtr last_msg_;
+    void visionCallback(const fake_ar_publisher::ARMarkerConstPtr& msg)
+    {
+        last_msg_ = msg;
+        ROS_INFO_STREAM(last_msg_->pose.pose);
+    }
+
+    ros::Subscriber ar_sub_;
+    fake_ar_publisher::ARMarkerConstPtr last_msg_;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  ros::init(argc, argv, "vision_node");
+    // This must be called before anything else ROS-related
+    ros::init(argc, argv, "vision_node");
 
-  ros::NodeHandle nh;
-  Localizer localizer (nh);
+    // Create a ROS node handle
+    ros::NodeHandle nh;
 
-  ROS_INFO("Vision node starting");
-  ros::spin();
+    // The Localizer class provides this node's ROS interfaces
+    Localizer localizer(nh);
+
+    ROS_INFO("Vision node starting");
+
+    // Don't exit the program.
+    ros::spin();
 }
