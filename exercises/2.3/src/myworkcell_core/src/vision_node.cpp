6632277e556527878ac85f5d1c0a7886b129e511
Jeremy Zoss <jzoss@swri.org>
2017-02-12 19:41:50 -0600

    update Exercise 2.3 to match wiki

--- exercises/2.3/src/myworkcell_core/src/vision_node.cpp
+++ exercises/2.3/src/myworkcell_core/src/vision_node.cpp
@@ -1,48 +1,56 @@
+/**
+**  Simple ROS Node
+**/
 #include <ros/ros.h>
-#include <std_msgs/String.h>
-#include <myworkcell_core/LocalizePart.h>
-#include <tf/transform_listener.h>
 #include <fake_ar_publisher/ARMarker.h>
+#include <myworkcell_core/LocalizePart.h>
 
 class Localizer
 {
 public:
-  Localizer(ros::NodeHandle& nh)
-  {
-    ar_sub_ = nh.subscribe<fake_ar_publisher::ARMarker>("ar_pose_marker", 1, 
-      &Localizer::visionCallback, this);
+    Localizer(ros::NodeHandle& nh)
+    {
+        ar_sub_ = nh.subscribe<fake_ar_publisher::ARMarker>("ar_pose_marker", 1,
+        &Localizer::visionCallback, this);
 
-    server_ = nh.advertiseService("localize_part", &Localizer::localizePart, this);
-  }
+        server_ = nh.advertiseService("localize_part", &Localizer::localizePart, this);
+    }
 
-  bool localizePart(myworkcell_core::LocalizePart::Request& req,
-                    myworkcell_core::LocalizePart::Response& res)
-  {
-    // Read last message
-    fake_ar_publisher::ARMarkerConstPtr p = last_msg_;  
-    if (!p) return false;
+    void visionCallback(const fake_ar_publisher::ARMarkerConstPtr& msg)
+    {
+        last_msg_ = msg;
+        //ROS_INFO_STREAM(last_msg_->pose.pose);
+    }
 
-    res.pose = p->pose.pose;
-    return true;
-  }
+    bool localizePart(myworkcell_core::LocalizePart::Request& req,
+                      myworkcell_core::LocalizePart::Response& res)
+    {
+      // Read last message
+      fake_ar_publisher::ARMarkerConstPtr p = last_msg_;
+      if (!p) return false;
 
-  void visionCallback(const fake_ar_publisher::ARMarkerConstPtr& msg)
-  {
-    last_msg_ = msg;
-  }
+      res.pose = p->pose.pose;
+      return true;
+    }
 
-  ros::Subscriber ar_sub_;
-  ros::ServiceServer server_;
-  fake_ar_publisher::ARMarkerConstPtr last_msg_;
+    ros::Subscriber ar_sub_;
+    fake_ar_publisher::ARMarkerConstPtr last_msg_;
+    ros::ServiceServer server_;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  ros::init(argc, argv, "vision_node");
+    // This must be called before anything else ROS-related
+    ros::init(argc, argv, "vision_node");
 
-  ros::NodeHandle nh;
-  Localizer localizer (nh);
+    // Create a ROS node handle
+    ros::NodeHandle nh;
 
-  ROS_INFO("Vision node starting");
-  ros::spin();
+    // The Localizer class provides this node's ROS interfaces
+    Localizer localizer(nh);
+
+    ROS_INFO("Vision node starting");
+
+    // Don't exit the program.
+    ros::spin();
 }
