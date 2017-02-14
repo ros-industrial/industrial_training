edfa7845d9fe802c6a734e11296b0bb084f91d2e
Jeremy Zoss <jzoss@swri.org>
2017-02-13 22:32:11 -0600

    update Exercise 4.0 to match wiki

--- exercises/4.0/src/myworkcell_core/src/myworkcell_node.cpp
+++ exercises/4.0/src/myworkcell_core/src/myworkcell_node.cpp
@@ -1,26 +1,12 @@
 #include <ros/ros.h>
 #include <myworkcell_core/LocalizePart.h>
-#include <moveit/move_group_interface/move_group.h>
-
-static geometry_msgs::Pose transformPose(const geometry_msgs::Pose& in)
-{
-  tf::Transform in_world;
-  tf::poseMsgToTF(in, in_world);
-
-  tf::Quaternion flip_z (tf::Vector3(1, 0, 0), M_PI);
-  tf::Transform flip (flip_z);
-
-  in_world = in_world * flip;
-
-  geometry_msgs::Pose msg;
-  tf::poseTFToMsg(in_world, msg);
-  return msg;
-}
+#include <tf/tf.h>
+#include <moveit/move_group_interface/move_group_interface.h>
 
 class ScanNPlan
 {
 public:
-  ScanNPlan(ros::NodeHandle& nh) : group_("manipulator")
+  ScanNPlan(ros::NodeHandle& nh)
   {
     vision_client_ = nh.serviceClient<myworkcell_core::LocalizePart>("localize_part");
   }
@@ -28,9 +14,12 @@ public:
   void start(const std::string& base_frame)
   {
     ROS_INFO("Attempting to localize part");
+
     // Localize the part
     myworkcell_core::LocalizePart srv;
     srv.request.base_frame = base_frame;
+    ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame);
+
     if (!vision_client_.call(srv))
     {
       ROS_ERROR("Could not localize part");
@@ -38,39 +27,43 @@ public:
     }
     ROS_INFO_STREAM("part localized: " << srv.response);
 
-    // Plan for robot to move to part    
-    group_.setPoseTarget(srv.response.pose);
-    group_.move();
-    ROS_INFO("Done moving, planning cart path");
+    geometry_msgs::Pose move_target = flipPose(srv.response.pose);
+
+    // Plan for robot to move to part
+    moveit::planning_interface::MoveGroupInterface move_group("manipulator");
+    move_group.setPoseTarget(move_target);
+    move_group.move();
+  }
+
+  geometry_msgs::Pose flipPose(const geometry_msgs::Pose& in) const
+  {
+    tf::Transform in_tf;
+    tf::poseMsgToTF(in, in_tf);
+    tf::Quaternion flip_rot(tf::Vector3(1, 0, 0), M_PI);
+    tf::Transform flipped = in_tf * tf::Transform(flip_rot);
+    geometry_msgs::Pose out;
+    tf::poseTFToMsg(flipped, out);
+    return out;
   }
 
 private:
   // Planning components
   ros::ServiceClient vision_client_;
-  moveit::planning_interface::MoveGroup group_;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "myworkcell_node");
   ros::NodeHandle nh;
-  ros::NodeHandle pnh ("~");
-  ros::AsyncSpinner async_spinner (1);
+  ros::NodeHandle private_node_handle("~");
 
-  // Hello World
-  ROS_INFO("Hello, World from a ROS Node");
+  ROS_INFO("ScanNPlan node has been initialized");
 
   std::string base_frame;
-  pnh.param<std::string>("base_frame", base_frame, "world");
-  
-  ROS_INFO_STREAM("Using user base_frame: " << base_frame);
-  ScanNPlan app (nh);
+  private_node_handle.param<std::string>("base_frame", base_frame, "world"); // parameter name, string object reference, default value
 
-  // Give time for vision node to start
-  // Prefer something like service::waitForExist() instead
-  ros::Duration(.5).sleep();
-
-  async_spinner.start();
+  ScanNPlan app(nh);
+  ros::Duration(.5).sleep();  // wait for the class to initialize
   app.start(base_frame);
 
   ros::spin();
