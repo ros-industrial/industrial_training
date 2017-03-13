cadb9f65bfebd5094b06fc2cc3ef296c4c0bde3c
geoffreychiou <geoffrey.chiou@gmail.com>
2017-03-13 16:21:42 -0500

    Copied includes over from solution workspace to template workspace for exercise 4.3

--- exercises/perception_ws/src/lesson_perception/src/perception_node.cpp
+++ exercises/perception_ws/src/lesson_perception/src/perception_node.cpp
@@ -8,15 +8,21 @@
 #include <pcl_conversions/pcl_conversions.h> //hydro
 #include "pcl_ros/transforms.h"
 
-//#include <pcl/filters/voxel_grid.h>
-//#include <pcl/filters/passthrough.h>
-//#include <pcl/sample_consensus/method_types.h>
-//#include <pcl/sample_consensus/model_types.h>
-//#include <pcl/segmentation/sac_segmentation.h>
-//#include <pcl/filters/extract_indices.h>
-//#include <pcl/segmentation/extract_clusters.h>
-//#include <pcl/kdtree/kdtree_flann.h>
-//#include <pcl/filters/statistical_outlier_removal.h>
+// #include <pcl/filters/voxel_grid.h>
+// #include <pcl/filters/passthrough.h>
+// #include <pcl/sample_consensus/method_types.h>
+// #include <pcl/sample_consensus/model_types.h>
+// #include <pcl/segmentation/sac_segmentation.h>
+// #include <pcl/filters/extract_indices.h>
+// #include <pcl/segmentation/extract_clusters.h>
+// #include <pcl/kdtree/kdtree_flann.h>
+
+// #include <visualization_msgs/Marker.h>
+// #include <pcl/filters/crop_box.h>
+// #include <pcl/filters/statistical_outlier_removal.h>
+// #include <tf_conversions/tf_eigen.h>
+// #include <pcl/segmentation/extract_polygonal_prism_data.h>
+// #include <pcl/surface/convex_hull.h>
 
 int main(int argc, char *argv[])
 {
