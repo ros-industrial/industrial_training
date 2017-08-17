42c79a3b63b2c0e7b36a2bb4bf5cb432558f2f0c
Levi Armstrong <levi.armstrong@gmail.com>
2017-08-17 13:54:15 -0500

    Add exercise 5.0

---
+++ exercises/5.0/src/ur10_demo_descartes/src/plugin_init.cpp
@@ -0,0 +1,13 @@
+/*
+ * plugin_init.cpp
+ *
+ *  Created on: Apr 15, 2015
+ *      Author: Jorge Nicho
+ */
+
+#include <ur10_demo_descartes/ur10_robot_model.h>
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(ur10_demo_descartes::UR10RobotModel,descartes_core::RobotModel)
+
+
+
