890da34de776ef18d2a1c3e9ae230bca5502701a
Levi Armstrong <levi.armstrong@gmail.com>
2017-10-05 09:12:26 -0500

    Update Document

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 author = 'Levi Armstrong'
 project = 'Industrial_Training'
 scv_show_banner = True
-scv_banner-main-ref = 'master'
-scv_whitelist_branches = ('master', 'kinetic')
+scv_banner_main_ref = 'kinetic'
+scv_whitelist_branches = ('kinetic', 'kinetic-devel')
 
