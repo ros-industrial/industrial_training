929f65a2fdb2d1bfd81b1f2fed986d619d2418c3
marip8 <michael.ripperger@gmail.com>
2019-02-25 19:17:53 -0600

    Updated documentation to ScanNPlan node

--- exercises/6.0/src/myworkcell_core/src/myworkcell_node.cpp
+++ exercises/6.0/src/myworkcell_core/src/myworkcell_node.cpp
@@ -3,6 +3,10 @@
 #include <tf/tf.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 
+/**
+* @brief The ScanNPlan class is a client of the vision and path plan servers.  The ScanNPLan class takes
+* these services, computes transforms and published commands to the robot.
+*/
 class ScanNPlan
 {
 public:
@@ -11,6 +15,17 @@ public:
     vision_client_ = nh.serviceClient<myworkcell_core::LocalizePart>("localize_part");
   }
 
+  /**
+   * @brief start performs the motion planning and execution functions of the ScanNPlan of
+   * the node. The start method makes a service request for a transform that
+   * localizes the part.  The start method moves the "manipulator"
+   * move group to the localization target.  The start method requests
+   * a cartesian path based on the localization target.  The start method
+   * sends the cartesian path to the actionlib client for execution, bypassig
+   * MoveIt!
+   * @param base_frame is a string that specifies the reference frame
+   * coordinate system.
+   */
   void start(const std::string& base_frame)
   {
     ROS_INFO("Attempting to localize part");
@@ -40,6 +55,13 @@ private:
   ros::ServiceClient vision_client_;
 };
 
+/**
+ * @brief main is the ros interface for the ScanNPlan Class
+ * @param argc ROS uses this to parse remapping arguments from the command line.
+ * @param argv ROS uses this to parse remapping arguments from the command line.
+ * @return ROS provides typical return codes, 0 or -1, depending on the
+ * execution.
+ */
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "myworkcell_node");
