48ae6d382956971f321beb0f255207c58ea82661
Jerit George <jerit.george@swri.org>
2019-09-12 14:42:49 -0500

    Move 2.3 files into new 7.1 directory

---
+++ 7.1/src/myworkcell_core/src/myworkcell_node.cpp
@@ -0,0 +1,50 @@
+#include <ros/ros.h>
+#include <myworkcell_core/LocalizePart.h>
+
+class ScanNPlan
+{
+public:
+  ScanNPlan(ros::NodeHandle& nh)
+  {
+    vision_client_ = nh.serviceClient<myworkcell_core::LocalizePart>("localize_part");
+  }
+
+  void start(const std::string& base_frame)
+  {
+    ROS_INFO("Attempting to localize part");
+    // Localize the part
+    myworkcell_core::LocalizePart srv;
+    srv.request.base_frame = base_frame;
+    ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame);
+
+    if (!vision_client_.call(srv))
+    {
+      ROS_ERROR("Could not localize part");
+      return;
+    }
+    ROS_INFO_STREAM("part localized: " << srv.response);
+  }
+
+private:
+  // Planning components
+  ros::ServiceClient vision_client_;
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "myworkcell_node");
+  ros::NodeHandle nh;
+  ros::NodeHandle private_node_handle("~");
+
+  ROS_INFO("ScanNPlan node has been initialized");
+
+  std::string base_frame;
+  private_node_handle.param<std::string>("base_frame", base_frame, "world"); // parameter name, string object reference, default value
+
+  ScanNPlan app(nh);
+
+  ros::Duration(.5).sleep();  // wait for the class to initialize
+  app.start(base_frame);
+
+  ros::spin();
+}
