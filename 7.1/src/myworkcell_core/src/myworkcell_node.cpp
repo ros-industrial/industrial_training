b0f8a188f86a37ae1bdbae8307a030043a4e87d7
Lewis, Colin T <colin.lewis@swri.org>
2019-09-19 09:33:24 -0500

    minor porting start

--- 7.1/src/myworkcell_core/src/myworkcell_node.cpp
+++ 7.1/src/myworkcell_core/src/myworkcell_node.cpp
@@ -1,20 +1,20 @@
 #include <rclcpp/rclcpp.hpp>
-#include <myworkcell_core/LocalizePart.h>
+#include <myworkcell_core/srv/LocalizePart.hpp>
 
 class ScanNPlan
 {
 public:
   ScanNPlan(rclcpp::NodeHandle& nh)
   {
-    vision_client_ = nh.serviceClient<myworkcell_core::LocalizePart>("localize_part");
+    auto vision_client_ = node->create_client<myworkcell_core::LocalizePart>("localize_part");
   }
 
   void start(const std::string& base_frame)
   {
     RCLCPP_INFO("Attempting to localize part");
     // Localize the part
-    myworkcell_core::LocalizePart srv;
-    srv.request.base_frame = base_frame;
+    myworkcell_core::srv::LocalizePart srvr; //srv changed to srvr to avoid ambiguity
+    srvr.request.base_frame = base_frame;
     RCLCPP_INFO("Requesting pose in base frame: %s", base_frame);
 
     if (!vision_client_.call(srv))
@@ -22,12 +22,12 @@ public:
       RCLCPP_ERROR("Could not localize part");
       return;
     }
-    RCLCPP_INFO("part localized: %s", srv.response);
+    RCLCPP_INFO("part localized: %s", srvr.response);
   }
 
 private:
-  // Planning components
-  ros::ServiceClient vision_client_;
+  //
+  rclcpp::client::Client vision_client_;
 };
 
 int main(int argc, char **argv)
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
   auto node = rclcpp::Node::make_shared("myworkcell_node");
   auto private_node = rclcpp::Node::make_shared("~");
 
-  RCLCPP_INFO("ScanNPlan node has been initialized");
+  RCLCPP_INFO(node->get_logger(), "ScanNPlan node has been initialized");
 
   std::string base_frame;
   private_node_handle.param<std::string>("base_frame", base_frame, "world"); // parameter name, string object reference, default value
--- 7.1/src/myworkcell_core/src/vision_node.cpp
+++ 7.1/src/myworkcell_core/src/vision_node.cpp
@@ -1,32 +1,33 @@
 /**
-**  Simple ROS Node
+**  Simple ROS2 Node; WIP
 **/
+
 #include <rclcpp/rclcpp.hpp>
-#include <fake_ar_publisher/ARMarker.h>
-#include <myworkcell_core/LocalizePart.h>
+#include <fake_ar_publisher_msgs/msg/ARMarker.hpp>
+#include <myworkcell_core/srv/LocalizePart.hpp>
 
 class Localizer
 {
 public:
-    Localizer(rclcpp::Node& nh)
+    Localizer(rclcpp::Node& node)
     {
-        ar_sub_ = nh.subscribe<fake_ar_publisher::ARMarker>("ar_pose_marker", 1,
+        ar_sub_ = node->create_subscription<fake_ar_publisher::msg::ARMarker>("ar_pose_marker", 1,
         &Localizer::visionCallback, this);
 
-        server_ = nh.advertiseService("localize_part", &Localizer::localizePart, this);
+        server_ = node->create_service("localize_part", &Localizer::localizePart, this);
     }
 
-    void visionCallback(const fake_ar_publisher::ARMarkerConstPtr& msg)
+    void visionCallback(const fake_ar_publisher::msg::ARMarkerConstPtr& msg)
     {
         last_msg_ = msg;
-        //ROS_INFO_STREAM(last_msg_->pose.pose);
+        RCLPP_INFO(node->get_logger(), last_msg_->pose.pose); //ros2 INFO_STREAM?
     }
 
-    bool localizePart(myworkcell_core::LocalizePart::Request& req,
-                      myworkcell_core::LocalizePart::Response& res)
+    bool localizePart(myworkcell_core::srv::LocalizePart::Request& req,
+                      myworkcell_core::srv::LocalizePart::Response& res) //server response & request in ROS2?
     {
       // Read last message
-      fake_ar_publisher::ARMarkerConstPtr p = last_msg_;
+      fake_ar_publisher::msg::ARMarkerConstPtr p = last_msg_;
       if (!p) return false;
 
       res.pose = p->pose.pose;
@@ -34,7 +35,7 @@ public:
     }
 
     ros::Subscriber ar_sub_;
-    fake_ar_publisher::ARMarkerConstPtr last_msg_;
+    fake_ar_publisher::msg::ARMarkerConstPtr last_msg_;
     ros::ServiceServer server_;
 };
 
@@ -43,14 +44,14 @@ int main(int argc, char* argv[])
     // This must be called before anything else ROS-related
     rclcpp::init(argc, argv);
 
-    // Create a ROS node handle
+    // Create a ROS node. There are not node handles in ros 2
     auto node = rclcpp::Node::make_shared("vision_node");
 
     // The Localizer class provides this node's ROS interfaces
     Localizer localizer(node);
 
-    RCLCPP_INFO("Vision node starting");
+    RCLCPP_INFO(node->get_logger(),"Vision node starting");
 
     // Don't exit the program.
-    rclcpp::spin();
+    rclcpp::spin_some(node);
 }
