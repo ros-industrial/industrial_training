6c4e49ce094ee34227bf6ee7e199555d1addce3c
Max Snippe <m.j.w.snippe@saxion.nl>
2020-07-24 09:45:11 +0200

    Remove theme from repo

--- gh_pages/conf.py
+++ gh_pages/conf.py
@@ -95,7 +95,6 @@ download_support = True
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = ['_themes',]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
