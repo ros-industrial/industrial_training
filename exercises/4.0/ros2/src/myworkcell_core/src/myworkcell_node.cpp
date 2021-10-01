2f18631d0f014104c7b01d2605a4b71ecb3ecfec
Jorge Nicho <jrgnichodevel@gmail.com>
2021-10-01 17:04:45 -0500

    updated exercise to use MoveItCpp to plan and execute

--- exercises/4.0/ros2/src/myworkcell_core/src/myworkcell_node.cpp
+++ exercises/4.0/ros2/src/myworkcell_core/src/myworkcell_node.cpp
@@ -2,12 +2,14 @@
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <myworkcell_core/srv/localize_part.hpp>
 
-#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/moveit_cpp/moveit_cpp.h>
+#include <moveit/moveit_cpp/planning_component.h>
 
 class ScanNPlan : public rclcpp::Node
 {
 public:
-  ScanNPlan() : Node("scan_n_plan", rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true))
+  ScanNPlan() :
+    Node("scan_n_plan", rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true))
   {
     if (! this->has_parameter("base_frame"))
     {
@@ -15,6 +17,21 @@ public:
     }
 
     vision_client_ = this->create_client<myworkcell_core::srv::LocalizePart>("localize_part");
+
+    RCLCPP_INFO(this->get_logger(),"Scan and Plan ready");
+  }
+
+  // MoveIt setup
+  void setup()
+  {
+    // Instantiate moveit_cpp
+    moveit_cpp_ = std::make_shared<moveit_cpp::MoveItCpp>(this->shared_from_this());
+
+    // Planning component associated with a single motion group
+    planning_component_ = std::make_shared<moveit_cpp::PlanningComponent>("manipulator", moveit_cpp_);
+
+    // Parameters set on this node
+    plan_parameters_.load(this->shared_from_this());
   }
 
   void start(const std::string& base_frame)
@@ -26,7 +43,6 @@ public:
     request->base_frame = base_frame;
 
     auto future = vision_client_->async_send_request(request);
-
     if (future.wait_for(std::chrono::seconds(3)) == std::future_status::timeout)
     {
       RCLCPP_ERROR(this->get_logger(), "Failed to receive LocalizePart service response");
@@ -49,21 +65,48 @@ public:
     move_target.header.frame_id = base_frame;
     move_target.pose = response->pose;
 
-    RCLCPP_INFO(this->get_logger(), "Creating move group interface");
-    moveit::planning_interface::MoveGroupInterface move_group(
-        this->shared_from_this(),
-        "manipulator");
+    // getting current state of robot from environment
+    if (!moveit_cpp_->getPlanningSceneMonitor()->requestPlanningSceneState())
+    {
+      RCLCPP_ERROR(this->get_logger(), "Failed to get planning scene");
+      return;
+    }
+    moveit::core::RobotStatePtr start_robot_state = moveit_cpp_->getCurrentState(2.0);
 
-    RCLCPP_INFO(this->get_logger(), "Setting move target pose");
-    move_group.setPoseTarget(move_target);
+    // setting start robot state
+    planning_component_->setStartState(*start_robot_state);
 
-    RCLCPP_INFO(this->get_logger(), "Ready to plan and move!");
-    move_group.move();
-    RCLCPP_INFO(this->get_logger(), "Move completed");
+    // Set motion goal of end effector link
+    std::string ee_link = moveit_cpp_->getRobotModel()->getJointModelGroup(
+        planning_component_->getPlanningGroupName())->getLinkModelNames().back();
+
+    RCLCPP_INFO(this->get_logger(), "Setting move target pose for tip link \"%s\"", ee_link.c_str());
+    planning_component_->setGoal(move_target, ee_link);
+
+    // Now we can plan!
+    moveit_cpp::PlanningComponent::PlanSolution plan_solution = planning_component_->plan(plan_parameters_);
+    if (!plan_solution)
+    {
+      RCLCPP_ERROR(this->get_logger(),"Failed to plan");
+      return;
+    }
+
+    // If planning succeeded, execute the returned trajectory
+    bool success = moveit_cpp_->execute("manipulator", plan_solution.trajectory, true);
+    if (!success)
+    {
+      RCLCPP_ERROR_STREAM(this->get_logger(), "Failed to execute trajectory");
+      return;
+    }
   }
 
 private:
   // Planning components
+  moveit_cpp::MoveItCppPtr moveit_cpp_;
+  moveit_cpp::PlanningComponentPtr planning_component_;
+  moveit_cpp::PlanningComponent::PlanRequestParameters plan_parameters_;
+
+  // perception interface
   rclcpp::Client<myworkcell_core::srv::LocalizePart>::SharedPtr vision_client_;
 };
 
@@ -88,6 +131,9 @@ int main(int argc, char **argv)
       rclcpp::spin(app);
     }
   };
+
+  // Perform MoveIt initialization
+  app->setup();
 
   // Run our application
   app->start(base_frame);
