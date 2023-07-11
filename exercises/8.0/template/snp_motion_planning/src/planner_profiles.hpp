e9260bbe2971b1185295097261f86f8708d32bb3
Tyler Marr <tylermarr17@gmail.com>
2023-07-10 22:29:24 -0500

    Removed profiles solution from template

--- exercises/8.0/template/snp_motion_planning/src/planner_profiles.hpp
+++ exercises/8.0/template/snp_motion_planning/src/planner_profiles.hpp
@@ -20,57 +20,21 @@ template <typename FloatType>
 typename tesseract_planning::DescartesDefaultPlanProfile<FloatType>::Ptr createDescartesPlanProfile()
 {
   auto profile = std::make_shared<tesseract_planning::DescartesDefaultPlanProfile<FloatType>>();
-  profile->num_threads = static_cast<int>(std::thread::hardware_concurrency());
-  profile->use_redundant_joint_solutions = false;
-  profile->allow_collision = false;
-  profile->enable_collision = true;
-  profile->enable_edge_collision = false;
 
-  // Use the default state and edge evaluators
-  profile->state_evaluator = nullptr;
-  profile->edge_evaluator = [](const tesseract_planning::DescartesProblem<FloatType> & /*prob*/) ->
-      typename descartes_light::EdgeEvaluator<FloatType>::Ptr {
-        auto eval = std::make_shared<descartes_light::CompoundEdgeEvaluator<FloatType>>();
-
-        // Nominal Euclidean distance
-        eval->evaluators.push_back(std::make_shared<descartes_light::EuclideanDistanceEdgeEvaluator<FloatType>>());
-
-        // Penalize wrist motion
-        //        Eigen::Matrix<FloatType, Eigen::Dynamic, 1> wrist_mask(prob.manip->numJoints());
-        //        FloatType weight = static_cast<FloatType>(5.0);
-        //        wrist_mask << 0.0, 0.0, 0.0, weight, weight, weight;
-        //        eval->evaluators.push_back(
-        //            std::make_shared<descartes_light::EuclideanDistanceEdgeEvaluator<FloatType>>(wrist_mask));
-
-        return eval;
-      };
-
-  profile->vertex_evaluator = nullptr;
-
-  profile->target_pose_sampler =
-      std::bind(tesseract_planning::sampleToolZAxis, std::placeholders::_1, 10.0 * M_PI / 180.0);
+  /* =======================
+     * Fill Code: DESCARTES
+     * =======================*/
 
   return profile;
 }
 
 tesseract_planning::OMPLDefaultPlanProfile::Ptr createOMPLProfile()
 {
-  // OMPL freespace and transition profiles
-  // Create the RRT parameters
-  auto n = static_cast<Eigen::Index>(std::thread::hardware_concurrency());
-  auto range = Eigen::VectorXd::LinSpaced(n, 0.05, 0.5);
-
-  // Add as many planners as available threads so mulitple OMPL plans can happen in parallel
   auto profile = std::make_shared<tesseract_planning::OMPLDefaultPlanProfile>();
-  profile->planning_time = 20.0;
-  profile->planners.clear();
-  profile->planners.reserve(static_cast<std::size_t>(n));
-  for (Eigen::Index i = 0; i < n; ++i)
-  {
-    auto rrt = std::make_shared<tesseract_planning::RRTConnectConfigurator>();
-    rrt->range = range(i);
-    profile->planners.push_back(rrt);
-  }
+
+  /* =======================
+     * Fill Code: OMPL
+     * =======================*/
 
   return profile;
 }
@@ -78,27 +42,21 @@ tesseract_planning::OMPLDefaultPlanProfile::Ptr createOMPLProfile()
 std::shared_ptr<tesseract_planning::TrajOptPlanProfile> createTrajOptToolZFreePlanProfile()
 {
   auto profile = std::make_shared<tesseract_planning::TrajOptDefaultPlanProfile>();
-  profile->cartesian_coeff = Eigen::VectorXd::Constant(6, 1, 5.0);
-  profile->cartesian_coeff(5) = 0.0;
+
+  /* =======================
+     * Fill Code: TRAJOPT PLAN
+     * =======================*/
+
   return profile;
 }
 
 std::shared_ptr<tesseract_planning::TrajOptDefaultCompositeProfile> createTrajOptProfile()
 {
-  // TrajOpt profiles
   auto profile = std::make_shared<tesseract_planning::TrajOptDefaultCompositeProfile>();
-  profile->smooth_velocities = true;
-  profile->velocity_coeff = Eigen::VectorXd::Constant(6, 1, 10.0);
-  profile->acceleration_coeff = Eigen::VectorXd::Constant(6, 1, 25.0);
-  profile->jerk_coeff = Eigen::VectorXd::Constant(6, 1, 50.0);
 
-  profile->collision_cost_config.enabled = true;
-  profile->collision_cost_config.type = trajopt::CollisionEvaluatorType::DISCRETE_CONTINUOUS;
-  profile->collision_cost_config.safety_margin = 0.010;
-  profile->collision_cost_config.safety_margin_buffer = 0.010;
-  profile->collision_cost_config.coeff = 10.0;
-
-  profile->collision_constraint_config.enabled = false;
+  /* =======================
+     * Fill Code: TRAJOPT COMPOSITE
+     * =======================*/
 
   return profile;
 }
