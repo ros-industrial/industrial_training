b88a34cc04091474d233263b2f7df3e29f308e55
jcampbell9 <josh.campbell@swri.org>
2024-07-21 11:45:07 -0500

    cleaning up before PR

--- exercises/Perception-Driven_Manipulation/ros2/solution_ws/install/pick_and_place_msgs/local/lib/python3.10/dist-packages/pick_and_place_msgs/action/__init__.py
+++
@@ -1,1 +0,0 @@
-from pick_and_place_msgs.action._execute_grasp_move import ExecuteGraspMove  # noqa: F401
--- exercises/Perception-Driven_Manipulation/ros2/solution_ws/install/pick_and_place_msgs/local/lib/python3.10/dist-packages/pick_and_place_msgs/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from pick_and_place_msgs.srv._get_target_pose import GetTargetPose  # noqa: F401
