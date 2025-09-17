6432165536f4a7336d06faab6e54d446c1dd3f2f
DavidMerzJr <dmmjr2@gmail.com>
2025-09-17 15:06:15 -0500

    Update Copyright Notice Date

--- gh_pages/conf.py
+++ gh_pages/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Industrial Training'
-copyright = u'2020, ROS-Industrial'
+copyright = u'2025, ROS-Industrial'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
